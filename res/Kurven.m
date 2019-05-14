38ccf9e397b655ab054c126231c629d3a5c2bdf4
LucasRomier <lucas.romier@gmail.com>
2019-05-14 17:21:49 +0200

    Finished Kondensatoren.
    Signed-off-by: LucasRomier <lucas.romier@gmail.com>

--- res/Kurven.m
+++ res/Kurven.m
@@ -32,3 +32,30 @@ title('Ladung und Entladung des Kondensators bei 1 Lichtschranke');
 xlabel('t in s'); 
 ylabel('U in V');
 saveas(f3, 'Kondensator_1_Lichtschranke_Messung', 'epsc');
+
+% Durchquerung schnell
+Durchquerung_schnell = readtable('Lichtschranke 2 schnell');
+f4 = figure;
+plot(Durchquerung_schnell.Zeit, Durchquerung_schnell.Kanal_1, 'LineWidth', 2);
+title('Schnelle Durquerung beider Lichtschranken');
+xlabel('t in s'); 
+ylabel('U in V');
+saveas(f4, 'Lichtschranke_2_schnell_Messung', 'epsc');
+
+% Durchquerung mittel
+Durchquerung_mittel = readtable('Lichtschranke 2 medium');
+f5 = figure;
+plot(Durchquerung_mittel.Zeit, Durchquerung_mittel.Kanal_1, 'LineWidth', 2);
+title('Mittlere Durquerung beider Lichtschranken');
+xlabel('t in s'); 
+ylabel('U in V');
+saveas(f5, 'Lichtschranke_2_medium_Messung', 'epsc');
+
+% Durchquerung langsam
+Durchquerung_langsam = readtable('Lichtschranke 2 langsam');
+f6 = figure;
+plot(Durchquerung_langsam.Zeit, Durchquerung_langsam.Kanal_1, 'LineWidth', 2);
+title('Langsame Durquerung beider Lichtschranken');
+xlabel('t in s'); 
+ylabel('U in V');
+saveas(f6, 'Lichtschranke_2_langsam_Messung', 'epsc');
